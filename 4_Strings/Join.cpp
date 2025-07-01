b1e10db4d596ed82f8d0e1977b490c8b75ab04db
KarlJungtow <k.jungtow@tu-berlin.de>
2025-07-01 15:24:37 +0200

    Likeylike

--- 4_Strings/Join.cpp
+++ 4_Strings/Join.cpp
@@ -27,12 +27,10 @@ std::vector<ResultRelation> performJoin(const std::vector<CastRelation>& castRel
 
     for(auto cast : castRelation) {
         for (auto title : titleRelation) {
-            if (cast.note[0] == title.title[0]) {
-                if (strncmp(cast.note, title.title, strlen(title.title))==0) {
+                if (strstr(cast.note, title.title) != nullptr) {
                     resultTuples.push_back(createResultTuple(cast, title));
                 }
             }
-        }
     }
     
     return resultTuples;
